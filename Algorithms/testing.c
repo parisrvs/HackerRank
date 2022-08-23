446836a038c4f1902bd46c03c09a767952e41fcf
Parijat Srivastava <parisrvs@hotmail.com>
2022-08-23 09:46:54 +0530

    3 start

--- Algorithms/testing.c
+++ Algorithms/testing.c
@@ -3,25 +3,31 @@
 #include "/home/paris/Codes/HackerRank/clibs/myalgo.h"
 
 
-int divisibleSumPairs(int n, int k, int ar_count, int* ar) {
-    int count = 0;
-    for (int x = 0; x < ar_count - 1; x++)
-        for (int y = x+1; y < ar_count; y++)
-            if (((ar[x] + ar[y]) % k) == 0)
-                count++;
+void bonAppetit(int bill_count, int* bill, int k, int b) {
+    int sum = 0;
+    for (int c = 0; c < bill_count; c++)
+        if (c != k)
+            sum += (*(bill+c));
     
-    return count;
+    if ((sum / 2) == b)
+        printf("Bon Appetit\n");
+    else
+        printf("%i\n", b - (sum/2));
 }
+
+
 
 int main(void) {
     int count = get_int("Count: ");
+    int k = get_int("K: ");
     int *arr = malloc(sizeof(int)*count);
     
     int c;
     for (c = 0; c < count; c++)
         *(arr+c) = get_int("Number: ");
     
-    int d = get_int("K ");
-    printf("%i\n", divisibleSumPairs(count, d, count, arr));
+    
+    int b = get_int("Charge: ");
+    bonAppetit(count, arr, k, b);
     free(arr);
 }
